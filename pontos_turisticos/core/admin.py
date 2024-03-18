ad5e4e457c3103ba472b6182fc971e66d95f51ac
iasmim <iasmim.dantas@softfocus.com.br>
2024-03-18 14:50:38 -0300

    create model core and api/serializers estructure

--- pontos_turisticos/core/admin.py
+++ pontos_turisticos/core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import PontoTuristico
 
-# Register your models here.
+admin.site.register(PontoTuristico)
+class OperacaoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'aprovado')
+    search_fields = ('nome')
